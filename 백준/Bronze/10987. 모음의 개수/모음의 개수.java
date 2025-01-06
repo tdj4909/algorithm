535a65ffa707aea77d0eb576199999b5e6ff708b
tdj4909 <52050824+tdj4909@users.noreply.github.com>
2025-01-06 17:08:08 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = 0;
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == 'a' ||
+                    s.charAt(i) == 'e' ||
+                    s.charAt(i) == 'i' ||
+                    s.charAt(i) == 'o' ||
+                    s.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        bw.write(count+"");
+        bw.close();
+        br.close();
+    }
+}
