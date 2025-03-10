92f191c0fa0587d6429188ccda845e9056b18f16
tdj4909 <52050824+tdj4909@users.noreply.github.com>
2025-03-10 09:37:15 +0900

    [level 1] Title: 폰켓몬, Time: 7.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        if (nums.length / 2 <= map.size()){
+            answer = nums.length / 2;
+        } else {
+            answer = map.size();
+        }
+        return answer;
+    }
+}
