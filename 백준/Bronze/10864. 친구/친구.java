f4446363d3e59090b6e4fbc2e17a71456ac9ce4e
tdj4909 <52050824+tdj4909@users.noreply.github.com>
2025-03-30 13:38:43 +0900

    [Bronze II] Title: 친구, Time: 144 ms, Memory: 14928 KB -BaekjoonHub

---
+++ 백준/Bronze/10864. 친구/친구.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] friends = new int[N+1];
+        for (int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            friends[A]++;
+            friends[B]++;
+        }
+
+        for (int i = 1; i <= N; i++){
+            System.out.println(friends[i]);
+        }
+    }
+}
