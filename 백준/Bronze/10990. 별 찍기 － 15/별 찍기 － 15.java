934f4234a68f5ffae5b9305d2d48f3a5590fe9da
tdj4909 <52050824+tdj4909@users.noreply.github.com>
2025-04-22 09:10:47 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 288 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N+i; j++){
+                if (j==N-i-1 || j==N+i-1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
